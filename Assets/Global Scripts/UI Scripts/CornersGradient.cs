40d8ccda52f0e020a1cae7f8297b7344a0e94764
Max <Maxxos@outlook.com>
2022-12-30 14:32:27 -0800

    Added Global use

---
+++ Assets/Global Scripts/UI Scripts/CornersGradient.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace PolygonPlanet.Gradient
+{
+    [AddComponentMenu("UI/Effects/Corners Gradient")]
+    public class CornersGradient : BaseMeshEffect
+    {
+        [Header("Variables")]
+        public Color topLeftColor;
+        public Color topRightColor;
+        public Color bottomLeftColor;
+        public Color bottomRightColor;
+
+        public override void ModifyMesh(VertexHelper vertextHelper)
+        {
+            if (enabled == true)
+            {
+                Rect rect = graphic.rectTransform.rect;
+                GradientUtils.Matrix2x3 localPositionMatrix = GradientUtils.LocalPositionMatrix(rect, Vector2.right);
+                UIVertex uiVertex = default(UIVertex);
+
+                for (int i = 0; i < vertextHelper.currentVertCount; i++)
+                {
+                    vertextHelper.PopulateUIVertex(ref uiVertex, i);
+                    Vector2 normalizedPosition = localPositionMatrix * uiVertex.position;
+                    uiVertex.color *= GradientUtils.Bilerp(bottomLeftColor, bottomRightColor, topLeftColor, topRightColor, normalizedPosition);
+                    vertextHelper.SetUIVertex(uiVertex, i);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Global Scripts/UI Scripts/Gradient.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+[AddComponentMenu("UI/Effects/Gradient")]
+public class Gradient : BaseMeshEffect
+{
+    [Header("Variables")]
+    public Color color1 = Color.white;
+    public Color color2 = Color.white;
+    [Range(-180f, 180f)]
+    public float angle;
+    public bool ignoreRatio;
+
+    public override void ModifyMesh(VertexHelper vertexHelper)
+    {
+        if (enabled == true)
+        {
+            Rect rect = graphic.rectTransform.rect;
+            Vector2 direction = GradientUtils.RotationDir(angle);
+            if (!ignoreRatio)
+            {
+                direction = GradientUtils.CompensateAspectRatio(rect, direction);
+            }
+
+            GradientUtils.Matrix2x3 localPositionMatrix = GradientUtils.LocalPositionMatrix(rect, direction);
+            UIVertex uiVertex = default(UIVertex);
+            for (int i = 0; i < vertexHelper.currentVertCount; i++)
+            {
+                vertexHelper.PopulateUIVertex(ref uiVertex, i);
+                Vector2 localPosition = localPositionMatrix * uiVertex.position;
+                uiVertex.color *= Color.Lerp(color2, color1, localPosition.y);
+                vertexHelper.SetUIVertex(uiVertex, i);
+            }
+        }
+    }
+}
