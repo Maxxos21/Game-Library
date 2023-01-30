b998b08afdb4ba6816c4730ff6689d117c2877a8
Max <Maxxos@outlook.com>
2023-01-30 11:14:08 -0800

    test

--- Assets/Games/Source/WIP/Pipe/Scripts/PipeManager.cs
+++ Assets/Games/Source/WIP/Pipe/Scripts/PipeManager.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class PipeManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] pipePrefabs;
+    [SerializeField] private GameObject[] pipePrefab;
+    private List<int> solution = new List<int>();
+    private List<int> currentArrangement = new List<int>();
+
+    private void Start()
+    {
+        GetPipeRotation();
+        RandomizePipeRotation();
+    }
+
+    private void RandomizePipeRotation()
+    {
+        foreach (GameObject pipe in pipePrefab)
+        {
+            PipeLevelCreator pipeLevelCreator = pipe.GetComponent<PipeLevelCreator>();
+            int rotation = Random.Range(0, 4);
+            pipeLevelCreator.rotation = rotation;
+            pipeLevelCreator.UpdateRotation();
+        }
+    }
+
+    private void GetPipeRotation()
+    {
+        foreach (GameObject pipe in pipePrefab)
+        {
+            PipeLevelCreator pipeLevelCreator = pipe.GetComponent<PipeLevelCreator>();
+            int rotation = pipeLevelCreator.rotation;
+            solution.Add(rotation);
+        }
+
+        string solutionString = string.Join(",", solution.ToArray());
+        Debug.Log(solutionString);
+    }
 }
--- Assets/Games/Source/WIP/Pipe/Scripts/PipeRotate.cs
+++ Assets/Games/Source/WIP/Pipe/Scripts/PipeRotate.cs
@@ -2,11 +2,6 @@ using UnityEngine;
 
 public class PipeRotate : MonoBehaviour
 {
-    void Start()
-    {
-        transform.Rotate(0, Random.Range(0, 4) * 90, 0);
-    }
-
     private void OnMouseOver()
     {
         if (Input.GetMouseButtonDown(1))
