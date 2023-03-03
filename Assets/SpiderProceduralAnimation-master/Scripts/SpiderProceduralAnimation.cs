06dd20a1d7711dddd2f9538a2314f323879d7c0d
Max <Maxxos@outlook.com>
2023-03-03 10:18:49 -0800

    asdf

--- Assets/SpiderProceduralAnimation-master/Scripts/SpiderProceduralAnimation.cs
+++ Assets/SpiderProceduralAnimation-master/Scripts/SpiderProceduralAnimation.cs
@@ -128,6 +128,7 @@ public class SpiderProceduralAnimation : MonoBehaviour
         {
             Vector3 v1 = legTargets[0].position - legTargets[1].position;
             Vector3 v2 = legTargets[2].position - legTargets[3].position;
+            
             Vector3 normal = Vector3.Cross(v1, v2).normalized;
             Vector3 up = Vector3.Lerp(lastBodyUp, normal, 1f / (float)(smoothness + 1));
             transform.up = up;
