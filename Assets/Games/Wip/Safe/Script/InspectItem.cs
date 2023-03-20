5a567cb6cdce3f1ffcdee9600d73691100fc1da0
Max <Maxxos@outlook.com>
2023-03-20 10:45:28 -0700

    touches

---
+++ Assets/Games/Wip/Safe/Script/InspectItem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class InspectItem : MonoBehaviour
+{
+
+    [SerializeField] private TMP_Text inspectText;
+
+
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
